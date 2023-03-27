efc8cb4a93be387a2a639ea792c1c4660fb26361
wonhk choi <wonhk01@naver.com>
2023-03-27 07:01:23 +0000

    다음영화순위

--- movie.py
+++ movie.py
@@ -1,25 +1,29 @@
+
 import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn',headers=headers)
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=cur',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 # print(soup)
 # 내가 원하는 위치에 있는 데이터 가져오기
-# 결과 = soup.select('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
+''
+여기는 여러줄로 설명할수 있어요
+또 써도 되요
+''
+#title = soup.select_one('#old_content > table > tbody > tr:nth-child(6) > td.title > div > a')
+#print(title.text)
 
-'''
-#오 그러면 이렇게 해도
-출력에도 상관이 없겠네
-??????
-'''
+# #old_content > table > tbody > tr
+
 trs = soup.select("#old_content > table > tbody > tr")
-#랭킹을 달아주기 위한 변수
+# print(trs)
+# 랭킹을 달아주기 위한 변수
 rank = 0
 for tr in trs:
     a_tag = tr.select_one("td.title > div > a")
-    # 만약에 a_tag가 None이 아니면
-    if a_tag is not None:
-        rank = rank + 1 
+    # 만약에 a_tag 가 None이 아니면 text를 출력
+        if a_tag is not None:
+        rank = rank + 1
         print(rank, a_tag.text)
